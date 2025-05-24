633f8b0c53bee879e260520217f620b9d94f04a4
lmalmud <lmalmud2@jh.edu>
2025-05-24 17:53:11 -0400

    created the schema

---
+++ lob-market-making-sim/src/lob_market_making_sim/io/schema.py
@@ -0,0 +1,42 @@
+'''
+schema.py
+Define the schema that will be used as files are imported.
+
+Dataclass Reference
+https://www.dataquest.io/blog/how-to-use-python-data-classes/#:~:text=In%20Python%2C%20a%20data%20class,a%20program%20or%20a%20system.
+LOBSTER Reference
+https://lobsterdata.com/info/DataStructure.php
+'''
+
+from enum import Enum               # Class to associate numbers with keywords
+from dataclasses import dataclass   # Class designed to only hold values
+
+class Direction(Enum): 
+    '''
+    Direction of trade as indicated in the message file.
+    '''
+    BUY = 1         # A buy order is initiated by a seller 
+    SELL= - 1       # A sell order is initiated by a buyer
+    
+class EventType(Enum):
+    '''
+    All possible event types as indicated in message file.
+    '''
+    ADD = 1                 # Submission of a new limit order
+    CANCEL = 2              # Cancellation (partial deletion of a limit order)
+    DELETE = 3              # Total deletion of a limit order
+    EXECUTE_VISIBLE = 4     # Execution of a visible limit order
+    EXECUTE_HIDDEN = 5      # Execution of a hidden limit order
+    CROSS = 6               # Inditates a cross/auction trade
+    
+@dataclass(slots=True)
+class OrderEvent:
+    '''
+    One event, as formatted in the message file.
+    '''
+    ts: int # nanoseconds since midnight
+    etype: EventType
+    oid: int # unique order id
+    direction: Direction
+    px: int # price in integer ticks
+    qty: int # shares after event
