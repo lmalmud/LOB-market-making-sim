196d6f0367630a56bfdef1a7797233d60eec0f55
lmalmud <lmalmud2@jh.edu>
2025-05-30 22:27:41 -0400

    started gym custom

--- lob-market-making-sim/src/lob_market_making_sim/models/rl/env.py
+++ lob-market-making-sim/src/lob_market_making_sim/models/rl/env.py
@@ -13,11 +13,24 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 
+from typing import Iterable
+from lob_market_making_sim.core.engine import ReplayEngine
+from lob_market_making_sim.core.order_book import OrderBookL1
+from lob_market_making_sim.io.schema import OrderEvent
+
 class LOBMarketMakerEnv(gym.Env):
     '''
     MDP for a day of trading.
     '''
-    def __init__(self):
+    def __init__(self, events: Iterable[OrderEvent], inventory_limit: int = 1000):
+
+        self.ob = OrderBookL1() # orderbook for internal use
+        self.events = events # list of events that will be processed throught the simulation
+
+        # inventory limiti is the maximum amount of security a market maker is willing
+        # to hold in their account (in order to control risk)
+        self.inventory_limit = inventory_limit
+
         self.observation_space = spaces.Box()
         self.action_space = spaces.Discrete()
 
